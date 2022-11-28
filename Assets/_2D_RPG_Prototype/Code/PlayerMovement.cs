3d458c4eb63db8bb7862fbadbfcf865c8fa83534
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-11-28 21:08:48 +0200

    Update PlayerMovement.cs

--- Assets/_2D_RPG_Prototype/Code/PlayerMovement.cs
+++ Assets/_2D_RPG_Prototype/Code/PlayerMovement.cs
@@ -1,15 +1,24 @@
+using Assets._2D_RPG_Prototype.Code;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
-    [SerializeField] private Rigidbody2D _rigidBody;
 
-    private Vector2 input;
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D _rigidBody;
+    [SerializeField] private PlayerAnimator _animator;
+
+    private const float MIN_INPUT = .1f;
+
+    [SerializeField] private Vector2 input;
 
     private void Update()
     {
-        input = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        _animator.SetMovementState(input.magnitude > MIN_INPUT);
+        _animator.SetMovementValue(input);
     }
 
     private void FixedUpdate()

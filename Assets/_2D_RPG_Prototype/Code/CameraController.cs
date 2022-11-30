da973c6531b11a83cf70ea27b0e7089744b122e3
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-11-30 16:56:49 +0200

    Add CameraController.

---
+++ Assets/_2D_RPG_Prototype/Code/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets._2D_RPG_Prototype.Code
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float _lerpSpeed = .125f;
+        [SerializeField] private PlayerLoader _playerLoader;
+
+        private Transform _target;
+
+        private void Start()
+        {
+            _target = _playerLoader.Player.transform;
+            transform.position = _target.position;
+        }
+
+        private void FixedUpdate() =>
+            transform.position = Vector3.Lerp(transform.position, _target.position, _lerpSpeed);
+    }
+}

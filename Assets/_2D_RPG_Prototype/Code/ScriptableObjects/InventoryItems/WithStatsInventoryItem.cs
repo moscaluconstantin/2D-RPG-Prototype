48e7a8d16870c36e07281679a53c1dea212e94e4
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-12-14 18:13:40 +0200

    Update WithStatsInventoryItem.cs

--- Assets/_2D_RPG_Prototype/Code/ScriptableObjects/InventoryItems/WithStatsInventoryItem.cs
+++ Assets/_2D_RPG_Prototype/Code/ScriptableObjects/InventoryItems/WithStatsInventoryItem.cs
@@ -14,10 +14,7 @@ namespace Assets._2D_RPG_Prototype.Code.ScriptableObjects.InventoryItems
             string description = base.GetDescription();
 
             for (int i = 0; i < _stats.Length; i++)
-            {
-                //bool contains = description.Contains($"_v{i}");
                 description = description.Replace($"_v{i}", _stats[i].Value.ToString());
-            }
 
             return description;
         }

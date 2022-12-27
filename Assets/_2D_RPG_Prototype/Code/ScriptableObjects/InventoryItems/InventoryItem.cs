745cb9f7a579b1e9660ebb441d99844f3c2cd247
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-12-27 16:39:43 +0200

    Add Glyphs.

--- Assets/_2D_RPG_Prototype/Code/ScriptableObjects/InventoryItems/InventoryItem.cs
+++ Assets/_2D_RPG_Prototype/Code/ScriptableObjects/InventoryItems/InventoryItem.cs
@@ -11,12 +11,17 @@ namespace Assets._2D_RPG_Prototype.Code.ScriptableObjects.InventoryItems
         [SerializeField] private string _name;
         [SerializeField] private string _description;
         [SerializeField] private Sprite _image;
+        [SerializeField] private int _glyphIndex;
 
         public int Id => _id;
         public string Name => _name;
         public Sprite Image => _image;
+        public string Glypth => $"<sprite={_glyphIndex}>";
 
         public virtual string GetDescription() =>
             _description.ToLower();
+
+        public override string ToString() =>
+            Glypth;
     }
 }

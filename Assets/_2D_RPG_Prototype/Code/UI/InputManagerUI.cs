1a50a854ffa797050b898257d723f4846d649152
moscaluconstantin <55128671+moscaluconstantin@users.noreply.github.com>
2022-12-28 18:20:30 +0200

    Add InputManagerUI.

---
+++ Assets/_2D_RPG_Prototype/Code/UI/InputManagerUI.cs
@@ -0,0 +1,30 @@
+﻿using Assets._2D_RPG_Prototype.Code.Infrastructure.Services.Implementations;
+using Assets._2D_RPG_Prototype.Code.UI.Menu;
+using UnityEngine;
+
+namespace Assets._2D_RPG_Prototype.Code.UI
+{
+    public class InputManagerUI : MonoBehaviour
+    {
+        [SerializeField] UIService _uiService;
+
+        private InGameMenu _inGameMenu;
+
+        private void Start() =>
+            _inGameMenu = _uiService.GetWindow<InGameMenu>();
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                if (_uiService.ActiveWindow is IControlableWindow window)
+                    window.Hide();
+                else
+                    _inGameMenu.Show();
+
+                return;
+            }
+
+        }
+    }
+}
